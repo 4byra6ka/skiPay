57e87164a7757998ed06c0fe1b79cf42d434e677
gau <gau@1jz.ru>
2023-09-07 03:11:06 +0300

    start config dev

---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+phone_validator = RegexValidator(r"^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$", "The phone number provided is invalid")
+
+
+class User(AbstractUser):
+    username = None
+    email = None
+    phone = models.CharField(max_length=16, validators=[phone_validator], unique=True)
+    is_active = models.BooleanField(default=True)
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
